0d9ae3caf6420a166a316b2a0a94d45e2d4cb6dd
PhanTinh101201 <tinhphan0207@gmail.com>
2024-07-29 09:39:34 +0700

    demo json server in vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,42 @@
-import LoadingPage from "@/components/loading";
+"use client";
 import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+interface FormValues {
+  title: string;
+  author: string;
+  content: string;
+}
 
 const HomePage = () => {
+  const { register, handleSubmit } = useForm<FormValues>();
+
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    fetch(`https://demo-fake-json.onrender.com/blogs`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+      });
+  };
+
   return (
     <div className="flex justify-center items-center h-[100vh]">
-      <LoadingPage />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register("title")} type="text" placeholder="Title" />
+        <br />
+        <input {...register("author")} type="text" placeholder="Author" />
+        <br />
+        <input {...register("content")} type="text" placeholder="Content" />
+
+        <button type="submit">submit</button>
+      </form>
     </div>
   );
 };
